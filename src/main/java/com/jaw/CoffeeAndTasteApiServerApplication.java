13282c5a82038b49982883bc86f3f79df6fd074d
seondongpyo <seondongpyo@gmail.com>
2022-05-13 23:10:23 +0900

    feat: create a project

---
+++ src/main/java/com/jaw/CoffeeAndTasteApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.jaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAndTasteApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeAndTasteApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaw/CoffeeAndTasteApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAndTasteApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
