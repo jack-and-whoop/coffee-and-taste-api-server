63407b16bfe6d45ebebc566d20faa2fcedb6d29b
seondongpyo <seondongpyo@gmail.com>
2022-06-03 22:58:26 +0900

    feat: 장바구니 생성 API 제거 - 일반적으로 장바구니만 생성하는 경우는 드묾 - 장바구니에 메뉴를 추가할 때, 장바구니가 없는 경우에만 생성되는 것이 자연스러워 보임

--- src/main/java/com/jaw/cart/ui/CartRestController.java
+++ src/main/java/com/jaw/cart/ui/CartRestController.java
@@ -1,16 +1,13 @@
 package com.jaw.cart.ui;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jaw.cart.application.CartService;
-import com.jaw.cart.domain.Cart;
 import com.jaw.cart.domain.CartMenu;
 
 import lombok.RequiredArgsConstructor;
@@ -20,13 +17,6 @@ import lombok.RequiredArgsConstructor;
 public class CartRestController {
 
 	private final CartService cartService;
-
-	@PostMapping("/members/{memberId}/cart")
-	public ResponseEntity<Cart> create(@PathVariable Long memberId) {
-		Cart cart = cartService.create(memberId);
-		return ResponseEntity.created(URI.create(String.format("/members/%d/cart", memberId)))
-			.body(cart);
-	}
 
 	@GetMapping("/members/{memberId}/cart")
 	public ResponseEntity<List<CartMenu>> findAll(@PathVariable Long memberId) {
