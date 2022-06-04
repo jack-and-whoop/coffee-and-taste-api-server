aff39451f29972b58874060f3f511b0ec9d75d8e
seondongpyo <seondongpyo@gmail.com>
2022-06-04 15:47:25 +0900

    refactor: 장바구니에 메뉴 추가 테스트 수정

--- src/test/java/com/jaw/cart/application/CartServiceTest.java
+++ src/test/java/com/jaw/cart/application/CartServiceTest.java
@@ -1,19 +1,18 @@
 package com.jaw.cart.application;
 
-import com.jaw.cart.domain.Cart;
-import com.jaw.cart.domain.CartMenu;
-import com.jaw.member.application.InMemoryMemberRepository;
-import com.jaw.member.domain.Member;
-import com.jaw.menu.domain.Menu;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jaw.cart.ui.CartMenuResponseDTO;
+import com.jaw.member.application.InMemoryMemberRepository;
+import com.jaw.member.domain.Member;
+import com.jaw.menu.domain.Menu;
 
 class CartServiceTest {
 
@@ -37,22 +36,16 @@ class CartServiceTest {
 		memberRepository.clear();
 	}
 
-	@DisplayName("회원의 장바구니를 생성한다.")
-	@Test
-	void create() {
-		Member member = memberRepository.save(member( "홍길동"));
-		Cart cart = cartService.create(member.getId());
-		assertThat(cart.getMember()).isEqualTo(member);
-	}
-
 	@DisplayName("장바구니에 메뉴를 추가한다.")
 	@Test
-	void add() {
+	void addMenu() {
 		Member member = memberRepository.save(member("고길동"));
-		Cart cart = cartService.create(member.getId());
+
 		cartService.addMenu(member.getId(), menu("바닐라 플랫 화이트", 5_900), 1);
 		cartService.addMenu(member.getId(), menu("아이스 카페 모카", 5_500), 1);
-		List<CartMenu> cartMenus = cartMenuRepository.findAllByCart(cart);
+
+		List<CartMenuResponseDTO> cartMenus = cartService.findAll(member.getId());
+
 		assertThat(cartMenus).hasSize(2);
 	}
 
