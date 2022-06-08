20e525b4b054ba3a60f24ef546562cc9cb1fe70b
seondongpyo <seondongpyo@gmail.com>
2022-06-08 22:15:29 +0900

    feat: 시큐리티 설정 클래스 추가

---
+++ src/main/java/com/jaw/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.jaw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.headers().frameOptions().disable();
+
+		http.authorizeRequests()
+				.antMatchers("/api/**")
+					.permitAll();
+
+		http.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.exceptionHandling()
+			.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+	}
+}
