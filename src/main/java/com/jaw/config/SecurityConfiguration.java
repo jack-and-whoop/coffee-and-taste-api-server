cb3d66d868cc6c30f6e17a4019714a94670dded5
seondongpyo <seondongpyo@gmail.com>
2022-06-09 00:38:15 +0900

    refactor: 순환 참조 방지를 위해 PasswordEncoder의 빈 등록 위치를 변경

--- src/main/java/com/jaw/config/SecurityConfiguration.java
+++ src/main/java/com/jaw/config/SecurityConfiguration.java
@@ -1,14 +1,11 @@
 package com.jaw.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import com.jaw.auth.AuthenticationErrorFilter;
@@ -41,10 +38,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		http.addFilter(new JwtAuthenticationFilter(authenticationManager(), authenticationService))
 			.addFilterBefore(new AuthenticationErrorFilter(), JwtAuthenticationFilter.class);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
