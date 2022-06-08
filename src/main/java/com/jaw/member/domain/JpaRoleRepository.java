8884cb8a8f39d99c9403e91ba5cd50f4b5c6067f
seondongpyo <seondongpyo@gmail.com>
2022-06-09 00:38:34 +0900

    feat: JpaRoleRepository가 RoleRepository를 상속하도록 변경

--- src/main/java/com/jaw/member/domain/JpaRoleRepository.java
+++ src/main/java/com/jaw/member/domain/JpaRoleRepository.java
@@ -2,5 +2,5 @@ package com.jaw.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaRoleRepository extends JpaRepository<Role, Long> {
+public interface JpaRoleRepository extends RoleRepository, JpaRepository<Role, Long> {
 }
