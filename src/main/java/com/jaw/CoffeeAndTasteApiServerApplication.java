17d9729d68ae62abc5ac46a93aa91313438f1a68
seondongpyo <seondongpyo@gmail.com>
2022-06-08 09:23:48 +0900

    feat: OpenAPI 서버 설정 추가

--- src/main/java/com/jaw/CoffeeAndTasteApiServerApplication.java
+++ src/main/java/com/jaw/CoffeeAndTasteApiServerApplication.java
@@ -1,11 +1,8 @@
 package com.jaw;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = @Server(url = "https://coffee-and-taste.kro.kr"))
 @SpringBootApplication
 public class CoffeeAndTasteApiServerApplication {
 
