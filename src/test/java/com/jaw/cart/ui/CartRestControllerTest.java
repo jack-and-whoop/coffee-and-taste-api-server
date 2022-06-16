e862cec6008f730cb749d549e5515ca62b27844b
seondongpyo <seondongpyo@gmail.com>
2022-06-17 01:45:34 +0900

    test: 장바구니 API 요청 시, 인증 토큰이 유효하지 않은 테스트 케이스 추가

--- src/test/java/com/jaw/cart/ui/CartRestControllerTest.java
+++ src/test/java/com/jaw/cart/ui/CartRestControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import com.jaw.AbstractControllerTest;
-import com.jaw.auth.JwtUtil;
 import com.jaw.cart.application.CartService;
 import com.jaw.member.domain.Member;
 import com.jaw.member.domain.MemberRepository;
@@ -23,6 +22,8 @@ import com.jaw.menu.domain.MenuRepository;
 class CartRestControllerTest extends AbstractControllerTest {
 
 	private static final String BASE_URI = "/api/members/{memberId}/cart";
+	private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.1Zx-1BRb0VJflU1JBYaP_FqrL6S53uRBn5DhYablbfw";
+	private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjF9.1Zx-1BRb0VJflU1JBYaP_FqrL6S53uRBn5DhYablbf0";
 
 	@Autowired
 	private MemberRepository memberRepository;
@@ -32,9 +33,6 @@ class CartRestControllerTest extends AbstractControllerTest {
 
 	@Autowired
 	private CartService cartService;
-
-	@Autowired
-	private JwtUtil jwtUtil;
 
 	private Member member;
 
@@ -56,10 +54,9 @@ class CartRestControllerTest extends AbstractControllerTest {
 		Menu americano = menuRepository.save(menu("아메리카노", 4_000));
 
 		CartMenuRequestDTO request = new CartMenuRequestDTO(americano.getId(), 1);
-		String token = jwtUtil.encode(member.getId());
 
 		mvc.perform(post(BASE_URI, member.getId())
-				.header("Authorization", "Bearer " + token)
+				.header("Authorization", "Bearer " + VALID_TOKEN)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(request)))
 			.andExpect(status().isCreated())
@@ -69,22 +66,42 @@ class CartRestControllerTest extends AbstractControllerTest {
 			.andExpect(jsonPath("$.count").value(1));
 	}
 
+	@DisplayName("장바구니에 메뉴 추가 요청 시, 인증 토큰이 유효하지 않을 경우 HTTP 401 응답을 내려준다.")
+	@Test
+	void addMenuWithoutToken() throws Exception {
+		Menu americano = menuRepository.save(menu("아메리카노", 4_000));
+
+		CartMenuRequestDTO request = new CartMenuRequestDTO(americano.getId(), 1);
+
+		mvc.perform(post(BASE_URI, member.getId())
+				.header("Authorization", "Bearer " + INVALID_TOKEN)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isUnauthorized());
+	}
+
 	@DisplayName("유효한 인증 토큰이 전달될 경우, 장바구니에 담긴 모든 메뉴를 조회할 수 있다.")
 	@Test
 	void findAllWithToken() throws Exception {
 		Menu icedAmericano = menuRepository.save(menu("아이스 아메리카노", 4_500));
 		Menu mangoBanana = menuRepository.save(menu("망고 바나나 블렌디드", 6_300));
 
-		String token = jwtUtil.encode(member.getId());
-
 		CartMenuResponseDTO icedAmericanoInCart = cartService.addMenu(member.getId(), icedAmericano.getId(), 1);
 		CartMenuResponseDTO mangoBananaInCart = cartService.addMenu(member.getId(), mangoBanana.getId(), 1);
 		List<CartMenuResponseDTO> responses = List.of(icedAmericanoInCart, mangoBananaInCart);
 
 		mvc.perform(get(BASE_URI, member.getId())
-				.header("Authorization", "Bearer " + token))
+				.header("Authorization", "Bearer " + VALID_TOKEN))
 			.andExpect(status().isOk())
 			.andExpect(content().json(objectMapper.writeValueAsString(responses)));
+	}
+
+	@DisplayName("장바구니에 담긴 모든 메뉴 조회 요청 시, 인증 토큰이 유효하지 않을 경우 HTTP 401 응답을 내려준다.")
+	@Test
+	void findAllWithoutToken() throws Exception {
+		mvc.perform(get(BASE_URI, member.getId())
+				.header("Authorization", "Bearer " + INVALID_TOKEN))
+			.andExpect(status().isUnauthorized());
 	}
 
 	private Menu menu(String name, long price) {
