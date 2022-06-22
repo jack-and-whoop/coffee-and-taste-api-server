65fdc6d4a67dc341714ecbf87e81d0d27e264cf7
seondongpyo <seondongpyo@gmail.com>
2022-06-22 21:42:45 +0900

    test: 새로운 주문 생성 테스트

---
+++ src/test/java/com/jaw/order/domain/OrderRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.jaw.order.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.jaw.member.domain.Member;
+import com.jaw.menu.domain.Menu;
+import com.jaw.order.application.InMemoryOrderMenuRepository;
+import com.jaw.order.application.InMemoryOrderRepository;
+
+class OrderRepositoryTest {
+
+	private OrderRepository orderRepository;
+	private OrderMenuRepository orderMenuRepository;
+
+	@BeforeEach
+	void setUp() {
+		orderRepository = new InMemoryOrderRepository();
+		orderMenuRepository = new InMemoryOrderMenuRepository();
+	}
+
+	@DisplayName("새로운 주문을 생성한다.")
+	@Test
+	void create() {
+		Menu menu = Menu.builder().build();
+		OrderMenu orderMenu = orderMenuRepository.save(new OrderMenu(menu, 1L));
+		Member member = Member.builder().build();
+		Order orderRequest = new Order(member);
+		orderRequest.addOrderMenu(orderMenu);
+
+		Order order = orderRepository.save(orderRequest);
+
+		assertThat(order.getMember()).isEqualTo(member);
+		assertThat(order.getOrderMenus()).contains(orderMenu);
+		assertThat(order.getOrderMenus().get(0).getOrder()).isEqualTo(orderRequest);
+	}
+}
