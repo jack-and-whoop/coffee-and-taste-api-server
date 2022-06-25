75e3064f9744fb5208012afd4f95594307602f87
seondongpyo <seondongpyo@gmail.com>
2022-06-25 12:38:36 +0900

    feat: 장바구니 주문 요청 및 응답을 처리할 DTO 객체 추가

---
+++ src/main/java/com/jaw/cart/ui/CartMenuOrderRequestDTO.java
@@ -0,0 +1,13 @@
+package com.jaw.cart.ui;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartMenuOrderRequestDTO {
+
+	private List<Long> cartMenuIds;
+}
---
+++ src/main/java/com/jaw/cart/ui/CartMenuOrderResponseDTO.java
@@ -0,0 +1,26 @@
+package com.jaw.cart.ui;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jaw.order.domain.Order;
+import com.jaw.order.ui.OrderMenuResponseDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartMenuOrderResponseDTO {
+
+	private Long id;
+	private List<OrderMenuResponseDTO> orderMenus;
+
+	public CartMenuOrderResponseDTO(Order order) {
+		this.id = order.getId();
+		this.orderMenus = order.getOrderMenus()
+			.stream()
+			.map(OrderMenuResponseDTO::new)
+			.collect(Collectors.toList());
+	}
+}
